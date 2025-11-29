45349eb533038ef4802351143670b4c04f8125d2
Pedrozaz <guilhermebarb0sa@proton.me>
2025-11-29 17:54:26 -0300

    Initial setup

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int socket(int domain, int type, int protocol) {
+    return 0;
+}
+
+int main(void) {
+}
